3520c8641c696af6d51b6ee436a795e7ccf01c58
NickRayF <niklpn11@gmail.com>
2026-02-18 10:54:16 +0300

    chande

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/FrontHack/">
+    <BrowserRouter basename="/Frontend/">
       <App />
     </BrowserRouter>
   </StrictMode>,
